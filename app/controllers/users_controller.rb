75a2534fd85ee8ddec07ab78ef6e7ff5f0c961c7
jacksonohallo <jacksonohallo@gmail.com>
2022-10-06 16:47:03 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
+
 class UsersController < ApplicationController
+
+    #get all users
+    def index
+        users = User.all
+        render json: users
+    end
+
+#staying logged in
+
+    def show
+        user = User.find_by(id: session[:user_id])
+         if user
+        render json: user
+        else
+        render json: { error: "Not authorized" }, status: :unauthorized
+        end
+    end
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-post '/login' ,to:"sessions#create"
-delete '/logout' ,to:"sessions#destroy"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
-#user route
-get '/me', to: 'users#show'
-
-
-
+  post '/login' ,to:"sessions#create"
+  delete '/logout' ,to:"sessions#destroy"
+  get "/me", to: "users#show"
+  get "/users", to: "users#index"
 end
 
 
